7db744552e7b80b3298c999f30e08b8529f6c8b2
vv6uos <wanamzz600@gmail.com>
2021-12-09 20:17:41 +0900

    GET DATA:추천도서 및 판매도서

--- server.js
+++ server.js
@@ -6,8 +6,83 @@ const port = 8080;
 app.use(express.json());
 app.use(cors());
 
+app.get("/bestsellers", (req, res) => {
+  res.send({
+    bestsellers: [
+      {
+        id: 1,
+        name: "개발자되기책",
+        price: 27000,
+        seller: "송은",
+      },
+      {
+        id: 2,
+        name: "개발자되기책",
+        price: 27000,
+        seller: "송은",
+      },
+      {
+        id: 3,
+        name: "개발자되기책",
+        price: 27000,
+        seller: "송은",
+      },
+      {
+        id: 4,
+        name: "개발자되기책",
+        price: 27000,
+        seller: "송은",
+      },
+      {
+        id: 5,
+        name: "개발자되기책",
+        price: 27000,
+        seller: "송은",
+      },
+    ],
+  });
+});
+
 app.get("/books", (req, res) => {
-  res.send("책 정보 json데이터입니다!");
+  res.send({
+    books: [
+      {
+        id: 1,
+        name: "개발자되기책",
+        price: 27000,
+        seller: "송은",
+      },
+      {
+        id: 2,
+        name: "개발자되기책",
+        price: 27000,
+        seller: "송은",
+      },
+      {
+        id: 3,
+        name: "개발자되기책",
+        price: 27000,
+        seller: "송은",
+      },
+      {
+        id: 4,
+        name: "개발자되기책",
+        price: 27000,
+        seller: "송은",
+      },
+      {
+        id: 5,
+        name: "개발자되기책",
+        price: 27000,
+        seller: "송은",
+      },
+    ],
+  });
+});
+
+app.post("/test", (req, res) => {
+  const body = req.body;
+  res.send("상품이 업로드 되었습니다");
 });
 
 app.listen(port, () => {
