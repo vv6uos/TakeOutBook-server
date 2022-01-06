ad6dc65994c656e44832a64c69f1de761bbc40fc
vv6uos <wanamzz600@gmail.com>
2022-01-06 16:37:17 +0900

    DB 환경 구축및설정

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var http = require("http"); // node 내장 모듈 불러옴
 var hostname = "127.0.0.1"; // localhost와 동일
 var port = 8080;
-
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
