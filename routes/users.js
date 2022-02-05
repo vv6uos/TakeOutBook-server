9f7508306e3229ccd6223ec5c283b9d8c2f09fe4
vv6uos <wanamzz600@gmail.com>
2022-02-05 23:18:46 +0900

    회원가입된 유저정보 user DB에 담기

--- routes/users.js
+++ routes/users.js
@@ -4,16 +4,39 @@ const models = require("../models");
 
 router.get("/", (req, res) => {
   models.User.findAll({
-    order: ["id"],
+    order: [["id"]],
     attributes: ["id", "user_id", "password"],
   })
     .then((result) => {
-      console.log("user정보 전달");
+      console.log("모든user정보 전달", result);
       res.json(result);
     })
     .catch((err) => {
       console.log(err);
       res.send("users DB를 불러오는데 실패했습니다.");
+    });
+});
+
+router.post("/", (req, res) => {
+  const body = req.body;
+  const { user_id, password, user_name, email, address, phoneNumber } = body;
+  models.User.create({
+    user_id,
+    user_name,
+    password,
+    email,
+    address,
+    phoneNumber,
+  })
+    .then((result) => {
+      console.log("회원정보등록결과 : ", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("유저정보 등록 실패 ");
     });
 });
 
