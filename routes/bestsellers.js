ead13cde140490d8e0067a3b2d1975dd03706e50
vv6uos <wanamzz600@gmail.com>
2022-02-23 07:26:01 +0900

    bestsellers API 책분야 재설정

--- routes/bestsellers.js
+++ routes/bestsellers.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const models = require("../models");
 
 const ALADIN_URL =
-  "https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbwanamzz1755003&QueryType=Bestseller&MaxResults=5&start=1&SearchTarget=Book&output=js&Version=20131101";
+  "https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbwanamzz1755003&QueryType=Bestseller&MaxResults=5&start=1&SearchTarget=Book&Cover=Big&CategoryId=7396&output=js&Version=20131101";
 const getBestSellers = async (request) => {
   let response;
   try {
