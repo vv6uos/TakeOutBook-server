871f0b290aade96c9dce288a66d59e0cb708cb41
vv6uos <wanamzz600@gmail.com>
2022-04-04 19:01:32 +0900

    TEST 세션 저장, 유저찾기 -usersession 오류를 찾기 위함

--- routes/test.js
+++ routes/test.js
@@ -21,14 +21,47 @@ router.use(
 );
 router.use(cookieParser());
 
-router.get("/", (req, res) => {
+router.get("/createSession", (req, res) => {
   req.session.TBOsession = {
     result: "결과",
   };
 
   req.session.save(() => {
-    res.json({ msg: "=>세션저장완료", session: req.session.TBOsession });
+    console.log("TEST세션저장완료");
+    res.json({ msg: "=>TEST세션저장완료", session: req.session.TBOsession });
   });
+});
+
+router.get("/loadSession", (req, res) => {
+  console.log("loadSession 시작------");
+  if (req.session.TBOsession) {
+    console.log("TBO세션 O");
+    res.send({ msg: "TBO세션이 있습니다.", session: req.session.TBOsession });
+  } else {
+    console.log("TBO세션 x");
+    res.send("TBO세션 x");
+  }
+});
+
+router.post("/findUser", (req, res) => {
+  console.log("AXIOS TEST");
+  const body = req.body;
+  const { user_id, password } = body;
+  console.log("TEST:유저찾기,POST VALUE: ", body);
+  models.User.findOne({
+    where: {
+      user_id,
+      password,
+    },
+  })
+    .then((result) => {
+      console.log("FIND USER TEST", result);
+      res.json({ msg: "FindUser post결과값", result });
+    })
+    .catch((err) => {
+      console.log("일치하는 UserData 없음", err);
+      res.send({ msg: "일치하는 userdata없음" });
+    });
 });
 
 module.exports = router;
