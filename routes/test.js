9ef9d1c6ca68e3a35ac222c3e1219937b3eca537
vv6uos <wanamzz600@gmail.com>
2022-04-05 01:00:07 +0900

    TEST same-site NONE 설정 취소

--- routes/test.js
+++ routes/test.js
@@ -1,8 +1,21 @@
 const express = require("express");
+// const redis = require("redis");
 const router = express.Router();
 const models = require("../models");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
+// const connectRedis = require("connect-redis");
+// const RedisStore = connectRedis(session);
+
+// const redisClient1 = redis.createClient({
+//   url: "redis://:p7fade2cb9ca46db931204b0f81286740738b744fd19fcf077010a3d79b6ebe01@ec2-44-196-70-155.compute-1.amazonaws.com:27729",
+// });
+
+// const redisStoreInfo = {
+//   logErrors: true,
+//   client: redisClient1,
+// };
+
 const prod = process.env.NODE_ENV === "production";
 
 router.use(
@@ -12,7 +25,7 @@ router.use(
     resave: false,
     saveUninitialized: true,
     cookie: {
-      // samesite: "none",
+      samesite: "none",
       secure: prod ? true : false,
       domain: prod && ".takeoutbook.kr",
       maxAge: 60 * 60 * 1000,
@@ -33,6 +46,7 @@ router.get("/createSession", (req, res) => {
   });
 });
 
+//세션 저장의 문제인지 살펴 볼 필요가 있다... 로컬에서는 가능...
 router.get("/loadSession", (req, res) => {
   console.log("loadSession 시작------");
   if (req.session.TBOsession) {
