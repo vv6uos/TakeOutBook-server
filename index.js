5b8782ce612496a6586073fd9b85ff6820b626da
vv6uos <wanamzz600@gmail.com>
2022-04-05 09:17:51 +0900

    FIX CORS OPT 오류 수정 "외부서버 연결실패 문제"

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const port = process.env.PORT || 8080;
 const models = require("./models");
 const router = require("./routes/index");
 dotenv.config();
-const whiteList = process.env.TBO_CLIENT_URL || "http://localhost:3000";
+const prod = process.env.NODE_ENV === "production";
+const whiteList = [process.env.TBO_CLIENT_URL, "http://localhost:3000"];
 
 const corsOpt = {
   origin: [whiteList],
@@ -20,11 +21,17 @@ app.use(cors(corsOpt));
 app.use(router);
 
 app.listen(port, () => {
-  console.log(`서버가 돌아갑니다`);
+  console.log(
+    `.................>>[배포단계?  `,
+    prod,
+    "]",
+    process.env.TBO_CLIENT_URL,
+    ":  SERVER START..........................."
+  );
   models.sequelize
     .sync()
     .then(() => {
-      console.log("DB연결성공");
+      console.log(".............DB연결성공.................");
     })
     .catch((err) => {
       console.log("DB연결실패", err);
