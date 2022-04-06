9a1466394b242dff45fd8172dd2b05cbf05282df
vv6uos <wanamzz600@gmail.com>
2022-04-06 12:13:25 +0900

    리팩토링 USER_SESSION -console 정리

--- routes/userSession.js
+++ routes/userSession.js
@@ -52,14 +52,16 @@ router.post("/create", (req, res) => {
     })
     .catch((err) => {
       console.log("===>가입된 회원없음");
-      res.send({ answer: false, msg: "아이디와 비밀번호 확인 부탁드립니다" });
+      res.json({ answer: false, msg: "아이디와 비밀번호 확인 부탁드립니다" });
     });
 });
 
 router.get("/", (req, res) => {
   console.log("GET=USER_SESSION REQUEST");
   req.session.reload(() => {
+    console.log("==>세션 불러오기");
     if (req.session.member) {
+      console.log("===>session.member 있음", req.session.member);
       models.User.findOne({
         where: {
           id: req.session.member.id,
@@ -67,27 +69,32 @@ router.get("/", (req, res) => {
       })
         .then((result) => {
           const member = result.dataValues;
+          console.log("====>User  찾음", member);
           req.session.member = {
             isLogin: true,
             id: req.session.member.id,
             name: member.user_name,
             isSubscriber: member.isSubscriber,
           };
-          console.log("userSession :", req.session);
           req.session.save(() => {
-            res.send({ answer: true, user: req.session.member });
+            console.log(
+              "=====>session.member 업데이트 완료",
+              req.session.member
+            );
+            res.json({ answer: true, user: req.session.member });
           });
         })
         .catch((err) => {
+          console.log("====>일치하는 User 없음");
           console.log("/userSession NOT FOUND ID ");
-          res.send({
+          res.json({
             answer: false,
             msg: "USERSESSION ERROR MESSAGE: 세션아이디와 일치하는 아이디가 없습니다",
           });
         });
     } else {
-      console.log("===>NOT EXIST USER_SESSION ");
-      res.send({
+      console.log("===>session.member 없음 ");
+      res.json({
         answer: false,
         msg: "USERSESSION ERROR MESSAGE: 유저 세션이 존재하지 않습니다.",
       });
@@ -98,12 +105,14 @@ router.get("/", (req, res) => {
 router.get("/delete", (req, res) => {
   console.log("GET=USER_SESSION/DELETE REQUEST");
   if (req.session.member) {
-    console.log("로그아웃......", req.session.member);
+    console.log("===>session.member 있음", req.session.member);
     req.session.destroy(() => {
-      res.json({ answer: true, logout: true });
+      console.log("====>session member 삭제");
+      res.json({ answer: true, msg: "유저세션이 삭제 되었습니다" });
     });
   } else {
-    res.send({ logout: false });
+    console.log("===>session member 없음");
+    res.json({ answer: false, msg: "로그아웃이 실패했습니다" });
   }
 });
 
